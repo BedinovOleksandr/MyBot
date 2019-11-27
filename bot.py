3c1c95ccf03fca824f0644a31c004b9abbbb6018
BedinovOleksandr <alekzzandr@gmail.com>
2019-11-27 17:45:34 +0200

    changed bot.polling() to pot.infinity_polling()

--- bot.py
+++ bot.py
@@ -1,11 +1,23 @@
+from time import sleep
+
 import telebot
 import DBfunctions
 
-bot = telebot.TeleBot('YOUR_TOKEN')
+bot = telebot.TeleBot('915271863:AAHpmWLQOEsHPbz226tceUMGetyfBQoCNCs')
+
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     DBfunctions.insert_user(list(vars(message.from_user).values()))
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
-bot.polling()
+
+
+bot.infinity_polling(True)
+'''try:
+    bot.polling()
+except OSError as e:
+    print(e)
+    bot.stop_polling()
+    sleep(5)
+    bot.polling()'''
