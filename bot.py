77ee4a47edf3b24ee1d6496358f14dc83b15876a
BedinovOleksandr <alekzzandr@gmail.com>
2019-11-28 16:46:48 +0200

    adding logger

--- bot.py
+++ bot.py
@@ -1,16 +1,40 @@
 from time import sleep
-
 import telebot
 import DBfunctions
+import logging
 
-bot = telebot.TeleBot('YOUR TOKEN')
+logging.basicConfig(filename='messegesLog.log', filemode='w', level='INFO')
 
+
+bot = telebot.TeleBot('YOUR_TOKEN')
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
+keyboard1.row('Привет', 'Стикер', 'Пока')
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    logging.info(message.text)
     DBfunctions.insert_user(list(vars(message.from_user).values()))
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+
+@bot.message_handler(content_types=['text'])
+def bot_send_text(message):
+    logging.info(message.text)
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Пока мой создатель')
+    elif message.text.lower() == 'стикер':
+        bot.send_sticker(message.chat.id, 'CAADAgADfgAD9wLID-usD1lCSOuqFgQ')
+    else:
+        bot.send_message(message.chat.id, 'Я понимаю только кнопки')
+
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
 
 
 while True:
